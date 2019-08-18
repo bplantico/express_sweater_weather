22a693f28c55e03fa4eb0bbe7abf04a2a0a22f78
Brian Plantico <bplantico@gmail.com>
2019-08-18 09:41:33 -0600

    Adds early check to Users sessions endpoint that both email and password are provided

--- routes/sessions.js
+++ routes/sessions.js
@@ -6,24 +6,29 @@ const User = require('../models').User
 
 /* POST to login a user */
 router.post('/', async (req, res) => {
- await User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-  .then( user => {
-    if (user && _passwordsMatch(req.body.password, user.passwordHash)) {
+  if (req.body.email && req.body.password) {
+    await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    .then( user => {
+      if (user && _passwordsMatch(req.body.password, user.passwordHash)) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify({apiKey: user.apiKey}));
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(401).send(JSON.stringify({error: "Check email address and password combination"}));
+      }
+    })
+    .catch(error => {
       res.setHeader("Content-Type", "application/json");
-      res.status(200).send(JSON.stringify({apiKey: user.apiKey}));
-    } else {
-      res.setHeader("Content-Type", "application/json");
-      res.status(401).send(JSON.stringify({error: "Check email address and password combination"}));
-    }
-  })
-  .catch(error => {
+      res.status(500).send({error})
+    });
+  } else {
     res.setHeader("Content-Type", "application/json");
-    res.status(500).send({error})
-  });
+    res.status(401).send(JSON.stringify({error: "Please provide an email and password."}));
+  }
 });
 
 const _passwordsMatch = function bcryptPasswords(sentPassword, savedPassword) {
--- spec/api/v1/sessions.spec.js
+++ spec/api/v1/sessions.spec.js
@@ -53,6 +53,25 @@ describe("POST /api/v1/sessions path", () => {
     expect(response.status).toBe(401);
   });
 
+  it("doesn't send User their API key if no password provided", async () => {
+    await User.create({
+      email: "user5@example.com",
+      passwordHash: bcrypt.hashSync("password", 14),
+      apiKey: "random5",
+      apiKeyActive: true
+    });
+
+    const response = await request(app)
+    .post("/api/v1/sessions")
+    .send({
+      email: "user5@example.com",
+      password: ""
+    });
+
+    expect(response.body).toHaveProperty("error");
+    expect(response.status).toBe(401);
+  });
+
   it("doesn't send an API key if User hasn't registered", async () => {
     const response = await request(app)
     .post("/api/v1/sessions")
