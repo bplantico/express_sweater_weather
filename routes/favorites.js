6169dcbbe69e3f21a83e32c30923fdefa429de6b
Brian Plantico <bplantico@gmail.com>
2019-08-19 00:46:18 -0600

    Adds ability for users to favorite a location by sending a valid API key and a location in the body of a POST request to api/v1/favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,33 +1,70 @@
-var express = require('express');
-var router = express.Router();
-const uuidv4 = require('uuid/v4');
-const bcrypt = require('bcrypt');
-const User = require('../models').User
+var express            = require('express');
+var router             = express.Router();
+const uuidv4           = require('uuid/v4');
+const bcrypt           = require('bcrypt');
+const User             = require('../models').User
+const FavoriteLocation = require('../models').FavoriteLocation
+const fetch            = require('node-fetch');
+require('dotenv').config()
 
 /* POST to add a new favorite location */
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  console.log(res.body);
+  let location = req.body.location;
+  let apiKey = req.body
+  if (req.body.location && req.body.apiKey) {
+    User.findOne({
+      where: {
+        apiKey: req.body.apiKey
+      }
+    })
+    .then( user => {
+      if (user && user.apiKeyActive) {
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_GEOCODE_API_KEY}&address=${req.body.location}`)
+        .then(res => res.text())
+        .then(body => JSON.parse(body)["results"][0])
+        .then(result => {
+          if (result) {
+            let formattedAddress = result["formatted_address"]
+            let lat = result["geometry"]["location"]["lat"]
+            let lng = result["geometry"]["location"]["lng"]
+            FavoriteLocation.create({
+              location: formattedAddress,
+              lat: lat,
+              lng: lng,
+              active: true,
+              UserId: user.id,
+            })
+            .then(fave => {
+              if (fave) {
+                res.setHeader("Content-Type", "application/json");
+                res.status(201).send(JSON.stringify({data: `${fave.location} has been added to your favorites`}));
+              } else {
+                res.setHeader("Content-Type", "application/json");
+                res.status(401).send(JSON.stringify({error: "Something went wrong."}));
+              }
+            })
+          } else {
+            res.setHeader("Content-Type", "application/json");
+            res.status(401).send(JSON.stringify({error: "We can't find that location. Please try again."}));
+          }
+        })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(409).send(JSON.stringify({ error: error }));
+      });
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(401).send(JSON.stringify({error: "Please provide a valid API key."}));
+      }
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(409).send(JSON.stringify({ error: error }));
+    });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send(JSON.stringify({error: "Please provide a valid API key and location in the request body."}));
+  }
 });
 
 module.exports = router;
-
-// if (req.body.password == req.body.passwordConfirmation) {
-//   User.create({
-//     email: req.body.email,
-//     passwordHash: bcrypt.hashSync(req.body.password, 14),
-//     apiKey: uuidv4(),
-//     apiKeyActive: true
-//   })
-//   .then(user => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(201).send(JSON.stringify({apiKey: user.apiKey}));
-//   })
-//   .catch(error => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(409).send(JSON.stringify({ error: error }));
-//   });
-// } else {
-//   res.setHeader("Content-Type", "application/json");
-//   res.status(400).send(JSON.stringify({ message: "Please check that your passwords match." }));
-// };
--- spec/api/v1/favorites.spec.js
+++ spec/api/v1/favorites.spec.js
@@ -1,7 +1,8 @@
-var shell = require("shelljs");
-var request = require("supertest");
-var app = require("../../../app");
-const User = require('../../../models').User
+var shell    = require("shelljs");
+var request  = require("supertest");
+var app      = require("../../../app");
+const User   = require('../../../models').User
+const bcrypt = require('bcrypt');
 
 describe("POST /api/v1/favorites path", () => {
 
@@ -15,22 +16,57 @@ describe("POST /api/v1/favorites path", () => {
     });
 
   it("adds a location to user's favorites", async () => {
-    const user = await request(app)
-    .post("/api/v1/users")
-    .send({
+    const user = await User.create({
       email: "user_1@example.com",
-      password: "password",
-      passwordConfirmation: "password"
+      passwordHash: bcrypt.hashSync("password", 14),
+      apiKey: "random_1",
+      apiKeyActive: true
     });
 
     const response = await request(app)
     .post("/api/v1/favorites")
     .send({
-      location: "Denver, CO",
-      apiKey: user.apiKey
+      location: "Istanbul",
+      apiKey: "random_1"
     });
 
     expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty("data");
+  });
+
+  it("doesn't add a location to user's favorites if bad API key", async () => {
+    const response = await request(app)
+    .post("/api/v1/favorites")
+    .send({
+      location: "Istanbul",
+      apiKey: "wrong"
+    });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("doesn't add a location to user's favorites if location missing", async () => {
+    const response = await request(app)
+    .post("/api/v1/favorites")
+    .send({
+      location: null,
+      apiKey: "random_1"
+    });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("doesn't add a location to user's favorites if location not found", async () => {
+    const response = await request(app)
+    .post("/api/v1/favorites")
+    .send({
+      location: "XXXXXXXXX",
+      apiKey: "random_1"
+    });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty("error");
   });
 });
